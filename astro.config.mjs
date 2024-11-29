6d5cb2eb4a0d8a46f324658925ea93fbcfe10e04
Marc Pont <mpont91@gmail.com>
2024-11-29 10:37:40 +0100

    Add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-// @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), vue()],
+  output: 'server',
+  adapter: vercel()
+});
