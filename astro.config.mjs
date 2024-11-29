7cea91f50c87ab0b84b232991631b92363d073d1
Marc Pont <mpont91@gmail.com>
2024-11-29 10:37:51 +0100

    run prettier format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: 'server',
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
